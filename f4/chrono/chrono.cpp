77b99a3fc2c8470fb5ed72457fa7752a95843578
Oleg Aushev <aushevom@gmail.com>
2023-11-28 17:53:50 +0300

    update system_clocks

--- f4/chrono/chrono.cpp
+++ f4/chrono/chrono.cpp
@@ -27,29 +27,6 @@ void system_clock::init() {
     NVIC_SetPriority(SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
     SysTick->VAL = 0UL;
     SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
-
-    set_initialized();
-}
-
-
-void system_clock::run_tasks() {
-    for (size_t i = 0; i < _tasks.size(); ++i) {
-        if (now() >= (_tasks[i].timepoint + _tasks[i].period)) {
-            if (_tasks[i].func(i) == TaskStatus::success) {
-                _tasks[i].timepoint = now();
-            }
-        }
-    }
-
-
-    if (_delayed_task_delay.count() != 0)
-    {
-        if (now() >= (_delayed_task_start + _delayed_task_delay))
-        {
-            _delayed_task();
-            _delayed_task_delay = std::chrono::milliseconds(0);
-        }
-    }
 }
 
 
--- f4/chrono/chrono.h
+++ f4/chrono/chrono.h
@@ -8,7 +8,7 @@
 #include "../system/system.h"
 #include <apm32f4xx_misc.h>
 #include <emblib/core.h>
-#include <emblib/staticvector.h>
+#include <emblib/static_vector.h>
 #include <array>
 #include <chrono>
 
@@ -22,13 +22,7 @@ namespace mcu {
 namespace chrono {
 
 
-enum class TaskStatus {
-    success,
-    fail
-};
-
-
-class system_clock : private emb::noncopyable, public emb::monostate<system_clock> {
+class system_clock {
     friend void ::SysTick_Handler();
 public:
     system_clock() = delete;
@@ -36,57 +30,9 @@ public:
 private:
     static inline volatile int64_t _time{0};
     static constexpr std::chrono::milliseconds time_step{1};
-    static constexpr size_t task_count_max{4};
-
-/* periodic tasks */
-private:
-    struct Task
-    {
-        std::chrono::milliseconds period;
-        std::chrono::milliseconds timepoint;
-        TaskStatus (*func)(size_t);
-    };
-    static TaskStatus empty_task(size_t) { return TaskStatus::success; }
-    static inline emb::static_vector<Task, task_count_max> _tasks;
-public:
-    static void add_task(TaskStatus (*func)(size_t), std::chrono::milliseconds period)
-    {
-        Task task = {period, now(), func};
-        _tasks.push_back(task);
-    }
-
-    static void set_task_period(size_t index, std::chrono::milliseconds period)
-    {
-        if (index < _tasks.size()) {
-            _tasks[index].period = period;
-        }
-    }
-private:
-    static inline std::chrono::milliseconds _delayed_task_start{0};
-    static inline std::chrono::milliseconds _delayed_task_delay{0};
-    static void empty_delayed_task() {}
-    static inline void (*_delayed_task)() = empty_delayed_task;
-public:
-    static void register_delayed_task(void (*task)(), std::chrono::milliseconds delay)
-    {
-        _delayed_task = task;
-        _delayed_task_delay = delay;
-        _delayed_task_start = now();
-    }
-
 public:
     static std::chrono::milliseconds now() { return std::chrono::milliseconds(_time); }
     static std::chrono::milliseconds step() { return time_step; }
-
-    static void reset() {
-        _time = 0;
-        for (size_t i = 0; i < _tasks.size(); ++i) {
-            _tasks[i].timepoint = now();
-        }
-    }
-
-    static void run_tasks(); 
-
 protected:
     static void on_interrupt() { _time += time_step.count(); }
 };
