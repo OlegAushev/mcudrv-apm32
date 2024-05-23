61c1c854815c443ffaea6a9ddece3c69279eef85
Oleg Aushev <aushevom@gmail.com>
2024-05-23 13:13:15 +0300

    apm32: add naive crc32 from uint32_t buf

--- f4/crc/crc.h
+++ f4/crc/crc.h
@@ -5,7 +5,8 @@
 #ifdef APM32F4xx
 
 
-#include <mcudrv/apm32/f4/apm32f4_base.h>
+#include <mcudrv/apm32/f4/system/system.h>
+#include <apm32f4xx_rcm.h>
 #include <apm32f4xx_crc.h>
 #include <cstddef>
 
@@ -14,8 +15,17 @@ namespace mcu {
 namespace crc {
 
 
+inline void init() {
+    RCM_EnableAHB1PeriphClock(RCM_AHB1_PERIPH_CRC);
+}
+
+
 inline uint32_t calc_crc32(const uint8_t* buf, size_t len) {
-    return 42; // TODO
+    CRC->CTRL_B.RST = BIT_SET;
+    for (auto i = 0uz; i < len; ++i) {
+        CRC->DATA = buf[i];
+    }
+    return CRC->DATA;
 }
 
 
