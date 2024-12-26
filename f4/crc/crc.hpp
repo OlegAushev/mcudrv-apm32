83ed2b754743aa076ea96ec949e9492d49a8a10a
Oleg Aushev <aushevom@gmail.com>
2024-12-26 15:04:53 +0300

    apm32 crc: add apm32-namespace

--- f4/crc/crc.hpp
+++ f4/crc/crc.hpp
@@ -1,24 +1,20 @@
 #pragma once
 
-
 #ifdef MCUDRV_APM32
 #ifdef APM32F4xx
 
+#include <apm32f4xx_crc.h>
+#include <apm32f4xx_rcm.h>
 
 #include <mcudrv/apm32/f4/system/system.h>
-#include <apm32f4xx_rcm.h>
-#include <apm32f4xx_crc.h>
+
 #include <cstddef>
 
-
 namespace mcu {
+namespace apm32 {
 namespace crc {
 
-
-inline void init() {
-    RCM_EnableAHB1PeriphClock(RCM_AHB1_PERIPH_CRC);
-}
-
+inline void init() { RCM_EnableAHB1PeriphClock(RCM_AHB1_PERIPH_CRC); }
 
 inline uint32_t calc_crc32(const uint8_t* buf, size_t len) {
     CRC->CTRL_B.RST = BIT_SET;
@@ -28,10 +24,9 @@ inline uint32_t calc_crc32(const uint8_t* buf, size_t len) {
     return CRC->DATA;
 }
 
-
 } // namespace crc
+} // namespace apm32
 } // namespace mcu
-
 
 #endif
 #endif
