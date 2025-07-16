41fee2e2fd0cd5e21e868591e0f34510f0c9ef3d
Oleg Aushev <aushevom@gmail.com>
2025-07-16 17:22:15 +0300

    rename

--- f4/util/util.hpp
+++ f4/util/util.hpp
@@ -10,9 +10,10 @@ namespace util {
 
 class temperature_sensor {
 public:
-  static float get_value(uint32_t adc_data) {
-    float volt{static_cast<float>(adc_data) * mcu::apm32::adc::vref() /
-               mcu::apm32::adc::nmax<float>()};
+  static float convert_to_temperature(uint32_t adc_data) {
+    float volt{
+        static_cast<float>(adc_data) * mcu::apm32::adc::vref() /
+        mcu::apm32::adc::nmax<float>()};
     return (volt - 0.7782f) / 0.0024f + 28.0f;
   }
 };
