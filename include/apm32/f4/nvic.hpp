6c5f14a5bb53fba42fbbc4a33ee0990c9165c3d1
Oleg Aushev <aushevom@gmail.com>
2026-01-25 02:09:10 +0300

    improve irq_guard

--- include/apm32/f4/nvic.hpp
+++ include/apm32/f4/nvic.hpp
@@ -15,26 +15,28 @@ namespace nvic {
 using irq_number = IRQn_Type;
 
 class irq_priority {
-  static constexpr uint8_t group_max{15};
-  static constexpr uint8_t sub_max{0};
-  uint8_t group_;
+public:
+  static constexpr uint8_t preempt_priority_max = 15;
+  static constexpr uint8_t sub_priority_max = 0;
+private:
+  uint8_t preempt_;
   uint8_t sub_;
 public:
-  explicit constexpr irq_priority(uint8_t group, uint8_t sub = 0)
-      : group_{std::clamp(group, uint8_t{0}, group_max)},
-        sub_{std::clamp(sub, uint8_t{0}, sub_max)} {
-    core::ensure(group <= group_max);
-    core::ensure(sub <= sub_max);
+  explicit constexpr irq_priority(uint8_t preempt_pri, uint8_t sub_pri = 0)
+      : preempt_{std::clamp(preempt_pri, uint8_t{0}, preempt_priority_max)},
+        sub_{std::clamp(sub_pri, uint8_t{0}, sub_priority_max)} {
+    core::ensure(preempt_pri <= preempt_priority_max);
+    core::ensure(sub_pri <= sub_priority_max);
 #ifdef SILICON_REVISION_A
-    core::ensure(emb::iseven(group));
+    core::ensure(emb::iseven(preempt_pri));
 #endif
   }
 
-  constexpr uint8_t group() const {
-    return group_;
+  constexpr uint8_t preempt_priority() const {
+    return preempt_;
   }
 
-  constexpr uint8_t sub() const {
+  constexpr uint8_t sub_priority() const {
     return sub_;
   }
 };
@@ -43,7 +45,11 @@ inline void set_irq_priority(irq_number irqn, irq_priority priority) {
   uint32_t prioritygroup = NVIC_GetPriorityGrouping();
   NVIC_SetPriority(
       irqn,
-      NVIC_EncodePriority(prioritygroup, priority.group(), priority.sub())
+      NVIC_EncodePriority(
+          prioritygroup,
+          priority.preempt_priority(),
+          priority.sub_priority()
+      )
   );
 }
 
@@ -67,14 +73,29 @@ inline void disable_interrupts() {
   __disable_irq();
 }
 
+enum class irq_lock_policy {
+  all,     // PRIMASK
+  priority // BASEPRI
+};
+
+template<
+    irq_lock_policy Policy = irq_lock_policy::all,
+    uint8_t PreemptThreshold = 0>
+  requires(PreemptThreshold <= irq_priority::preempt_priority_max)
 class irq_guard {
 public:
-  irq_guard() : primask_backup_(__get_PRIMASK()) {
-    __disable_irq();
+  irq_guard() {
+    if constexpr (Policy == irq_lock_policy::all) {
+      backup_ = __get_PRIMASK();
+      __disable_irq();
+    } else {
+      backup_ = __get_BASEPRI();
+      __set_BASEPRI(encode_basepri());
+    }
   }
 
-  irq_guard(const irq_guard&) = delete;
-  irq_guard& operator=(const irq_guard&) = delete;
+  irq_guard(irq_guard const&) = delete;
+  irq_guard& operator=(irq_guard const&) = delete;
 
   ~irq_guard() {
     unlock();
@@ -83,14 +104,22 @@ public:
   void unlock() {
     if (locked_) {
       locked_ = false;
-      if (primask_backup_ == 0) {
-        __enable_irq();
+      if constexpr (Policy == irq_lock_policy::all) {
+        if (backup_ == 0) {
+          __enable_irq();
+        }
+      } else {
+        __set_BASEPRI(backup_);
       }
     }
   }
 private:
-  uint32_t primask_backup_;
+  uint32_t backup_;
   bool locked_ = true;
+
+  static constexpr uint32_t encode_basepri() {
+    return PreemptThreshold << (8 - __NVIC_PRIO_BITS);
+  }
 };
 
 } // namespace nvic
