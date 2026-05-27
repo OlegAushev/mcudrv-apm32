80b462e10522d5cddd553d3bb9c266e07b2a865d
Oleg Aushev <aushevom@gmail.com>
2026-05-27 16:01:27 +0300

    rcc: add constexpr clock tree with compile-time validation

---
+++ apm32/f4/rcc/rcc.hpp
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <apm32/device.hpp>
+
+#include <apm32/f4/rcc/rcc_types.hpp>
+#include <rcc_config.hpp>
+
+#ifndef HSE_VALUE
+#error "HSE_VALUE not defined"
+#endif
+
+namespace apm32::f4::rcc {
+
+inline constexpr std::uint64_t hse_value = HSE_VALUE;
+inline constexpr std::uint64_t hsi_value = 16'000'000;
+
+inline constexpr std::uint64_t hse_min = 4'000'000;
+inline constexpr std::uint64_t hse_max = 26'000'000;
+inline constexpr std::uint64_t vco_in_min = 1'000'000;
+inline constexpr std::uint64_t vco_in_max = 2'000'000;
+inline constexpr std::uint64_t vco_out_min = 100'000'000;
+inline constexpr std::uint64_t vco_out_max = 432'000'000;
+inline constexpr std::uint64_t sysclk_max = 168'000'000;
+inline constexpr std::uint64_t hclk_max = 168'000'000;
+inline constexpr std::uint64_t pclk1_max = 42'000'000;
+inline constexpr std::uint64_t pclk2_max = 84'000'000;
+
+template<typename T>
+constexpr T hse_frequency() {
+  return T(hse_value);
+}
+
+template<typename T>
+constexpr T hsi_frequency() {
+  return T(hsi_value);
+}
+
+template<typename T>
+constexpr T sysclk_frequency() {
+  using C = clock_config;
+  if constexpr (C::sysclk_src == sysclk_src::hse) {
+    return hse_frequency<T>();
+  } else if constexpr (C::sysclk_src == sysclk_src::hsi) {
+    return hsi_frequency<T>();
+  } else {
+    constexpr uint64_t pll_in = C::pll_src == pll_src::hse ? hse_value
+                                                           : hsi_value;
+
+    static_assert(
+        pll_in % C::pllb_div == 0,
+        "PLL VCO input frequency is not an integer number of Hz"
+    );
+    constexpr uint64_t vco_in = pll_in / C::pllb_div;
+    static_assert(
+        vco_in >= vco_in_min && vco_in <= vco_in_max,
+        "PLL VCO input frequency out of range"
+    );
+
+    constexpr uint64_t vco_out = vco_in * C::pll1a_mult;
+    static_assert(
+        vco_out >= vco_out_min && vco_out <= vco_out_max,
+        "PLL VCO output frequency out of range"
+    );
+
+    static_assert(
+        vco_out % to_divisor(C::pll1c_div) == 0,
+        "PLL output frequency is not an integer number of Hz"
+    );
+    constexpr uint64_t pll_out = vco_out / to_divisor(C::pll1c_div);
+
+    return T(pll_out);
+  }
+}
+
+template<typename T>
+constexpr T hclk_frequency() {
+  using C = clock_config;
+  return sysclk_frequency<T>() / to_divisor(C::ahb_div);
+}
+
+template<typename T>
+constexpr T cpu_frequency() {
+  return hclk_frequency<T>();
+}
+
+template<typename T>
+constexpr T pclk1_frequency() {
+  using C = clock_config;
+  return hclk_frequency<T>() / to_divisor(C::apb1_div);
+}
+
+template<typename T>
+constexpr T pclk1_timer_frequency() {
+  using C = clock_config;
+  return C::apb1_div == apb_div::_1 ? pclk1_frequency<T>()
+                                    : pclk1_frequency<T>() * 2;
+}
+
+template<typename T>
+constexpr T pclk2_frequency() {
+  using C = clock_config;
+  return hclk_frequency<T>() / to_divisor(C::apb2_div);
+}
+
+template<typename T>
+constexpr T pclk2_timer_frequency() {
+  using C = clock_config;
+  return C::apb2_div == apb_div::_1 ? pclk2_frequency<T>()
+                                    : pclk2_frequency<T>() * 2;
+}
+
+// ---- Clock Config Validation ----
+
+static_assert(
+    clock_config::pllb_div >= 2 && clock_config::pllb_div <= 63,
+    "pllb_div must be in 2..63"
+);
+
+static_assert(
+    clock_config::pll1a_mult >= 50 && clock_config::pll1a_mult <= 432,
+    "pll1a_mult must be in 50..432"
+);
+
+static_assert(
+    clock_config::plld_div >= 2 && clock_config::plld_div <= 15,
+    "plld_div must be in 2..15"
+);
+
+static_assert(
+    hse_value >= hse_min && hse_value <= hse_max,
+    "HSE_VALUE out of supported range"
+);
+
+static_assert(
+    sysclk_frequency<std::uint64_t>() % to_divisor(clock_config::ahb_div) == 0,
+    "HCLK frequency is not an integer number of Hz"
+);
+
+static_assert(
+    hclk_frequency<std::uint64_t>() % to_divisor(clock_config::apb1_div) == 0,
+    "PCLK1 frequency is not an integer number of Hz"
+);
+
+static_assert(
+    hclk_frequency<std::uint64_t>() % to_divisor(clock_config::apb2_div) == 0,
+    "PCLK2 frequency is not an integer number of Hz"
+);
+
+static_assert(
+    sysclk_frequency<std::uint64_t>() <= sysclk_max,
+    "SYSCLK frequency exceeds maximum"
+);
+
+static_assert(
+    hclk_frequency<std::uint64_t>() <= hclk_max,
+    "HCLK frequency exceeds maximum"
+);
+
+static_assert(
+    cpu_frequency<decltype(clock_config::expected_cpu_frequency)>()
+        == clock_config::expected_cpu_frequency,
+    "computed CPU frequency does not match expected CPU frequency"
+);
+
+static_assert(
+    pclk1_frequency<std::uint64_t>() <= pclk1_max,
+    "PCLK1 frequency exceeds maximum"
+);
+
+static_assert(
+    pclk2_frequency<std::uint64_t>() <= pclk2_max,
+    "PCLK2 frequency exceeds maximum"
+);
+
+} // namespace apm32::f4::rcc
---
+++ apm32/f4/rcc/rcc_types.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <cstdint>
+
+namespace apm32::f4::rcc {
+
+enum class sysclk_src : std::uint32_t { hse, hsi, pll };
+
+enum class pll_src : std::uint32_t { hse, hsi };
+
+enum class pll1c_div : std::uint32_t {
+  _2 = 0b00,
+  _4 = 0b01,
+  _6 = 0b10,
+  _8 = 0b11
+};
+
+enum class ahb_div : std::uint32_t {
+  _1   = 0b0000,
+  _2   = 0b1000,
+  _4   = 0b1001,
+  _8   = 0b1010,
+  _16  = 0b1011,
+  _64  = 0b1100,
+  _128 = 0b1101,
+  _256 = 0b1110,
+  _512 = 0b1111,
+};
+
+enum class apb_div : std::uint32_t {
+  _1  = 0b000,
+  _2  = 0b100,
+  _4  = 0b101,
+  _8  = 0b110,
+  _16 = 0b111,
+};
+
+constexpr std::uint32_t to_divisor(pll1c_div v) {
+  switch (v) {
+    case pll1c_div::_2: return 2;
+    case pll1c_div::_4: return 4;
+    case pll1c_div::_6: return 6;
+    case pll1c_div::_8: return 8;
+  }
+}
+
+constexpr std::uint32_t to_divisor(ahb_div v) {
+  switch (v) {
+    case ahb_div::_1:   return 1;
+    case ahb_div::_2:   return 2;
+    case ahb_div::_4:   return 4;
+    case ahb_div::_8:   return 8;
+    case ahb_div::_16:  return 16;
+    case ahb_div::_64:  return 64;
+    case ahb_div::_128: return 128;
+    case ahb_div::_256: return 256;
+    case ahb_div::_512: return 512;
+  }
+}
+
+constexpr std::uint32_t to_divisor(apb_div v) {
+  switch (v) {
+    case apb_div::_1: return 1;
+    case apb_div::_2: return 2;
+    case apb_div::_4: return 4;
+    case apb_div::_8: return 8;
+    case apb_div::_16: return 16;
+  }
+}
+
+} // namespace apm32::f4::rcc
